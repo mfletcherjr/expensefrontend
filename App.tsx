5e71364de9e14916db8e457a1e066dcb4b0ba0f5
Micheal Fletcher Jr <96737545+mfletcherjr@users.noreply.github.com>
2022-01-29 15:07:41 -0500

    Update App.tsx
    initial commit

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4b71bd',
     alignItems: 'center',
     justifyContent: 'center',
   },
